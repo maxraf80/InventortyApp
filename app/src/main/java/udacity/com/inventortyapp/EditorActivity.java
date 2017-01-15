7b0c46402acef25bc715b1e2ddc08c167399bd0e
Rafael <maxraf80@gmail.com>
2017-01-15 21:22:45 +0100

    21:22

--- app/src/main/java/udacity/com/inventortyapp/EditorActivity.java
+++ app/src/main/java/udacity/com/inventortyapp/EditorActivity.java
@@ -7,12 +7,23 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import udacity.com.inventortyapp.data.ItemContract;
 
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int EXISTING_ITEM_LOADER=0;
     private Uri mCurrentItemUri;
     private EditText mNameEditText;
+    private EditText mReferenceText;
+    private Spinner mCategorySpinner;
+    private EditText mPriceText;
+    private EditText mUnitsText;
+    private boolean mItemHasChanged = false;
+
+    private int mCategory= ItemContract.ItemEntry.CATEGORY_UNKNOWN;
+
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
