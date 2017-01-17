37798ea07a06ca47d52dcdc1b7f7f6d55e3df58e
Rafael <maxraf80@gmail.com>
2017-01-17 20:19:10 +0100

    20:19

--- app/src/main/java/udacity/com/inventortyapp/data/ItemContract.java
+++ app/src/main/java/udacity/com/inventortyapp/data/ItemContract.java
@@ -7,29 +7,29 @@ import android.provider.BaseColumns;
 public class ItemContract {
     private ItemContract(){}
 
-    public static final String CONTENT_AUTHORITY ="com.example.android.items";
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
-    public static final String PATH_ITEMS ="items";
-    public static final class  ItemEntry implements BaseColumns{
-    public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_ITEMS);
-    public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +CONTENT_AUTHORITY +"/"+
-    PATH_ITEMS;
+public static final String CONTENT_AUTHORITY ="com.example.android.items";
+public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+public static final String PATH_ITEMS ="items";
+public static final class  ItemEntry implements BaseColumns{
+public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_ITEMS);
+public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +CONTENT_AUTHORITY +"/"+ PATH_ITEMS;
+public static final String CONTENT_ITEM_TYPE =ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
 
-    public final static String TABLE_NAME = "items";
-    public final static String _ID = BaseColumns._ID;
-    public final static String COLUMN_ITEM_PRODUCT = "name";
-    public final static String COLUMN_ITEM_REFERENCE = "reference";
-    public final static String COLUMN_ITEM_CATEGORY = "category";
-    public final static String COLUMN_ITEM_PRICE = "price";
-    public final static String COLUMN_ITEM_UNITS = "units";
+public final static String TABLE_NAME = "items";
+public final static String _ID = BaseColumns._ID;
+public final static String COLUMN_ITEM_PRODUCT = "name";
+public final static String COLUMN_ITEM_REFERENCE = "reference";
+public final static String COLUMN_ITEM_CATEGORY = "category";
+public final static String COLUMN_ITEM_PRICE = "price";
+public final static String COLUMN_ITEM_UNITS = "units";
 
-    public static final int CATEGORY_UNKNOWN = 0;
-    public static final int CATEGORY_CLEANERS = 1;
-    public static final int CATEGORY_FOOD = 2;
-    public static final int CATEGORY_PETS = 3;
-    public static final int CATEGORY_HYGIENE = 4;
-    public static final int CATEGORY_FRUIT = 5;
-    public static final int CATEGORY_FISH = 6;
+public static final int CATEGORY_UNKNOWN = 0;
+public static final int CATEGORY_CLEANERS = 1;
+public static final int CATEGORY_FOOD = 2;
+public static final int CATEGORY_PETS = 3;
+public static final int CATEGORY_HYGIENE = 4;
+public static final int CATEGORY_FRUIT = 5;
+public static final int CATEGORY_FISH = 6;
 
     public static boolean isValidCategory(int category) {
         if (category == CATEGORY_UNKNOWN || category == CATEGORY_CLEANERS || category == CATEGORY_FOOD ||
--- app/src/main/java/udacity/com/inventortyapp/data/ItemProvider.java
+++ app/src/main/java/udacity/com/inventortyapp/data/ItemProvider.java
@@ -7,6 +7,9 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
+
+import static android.R.attr.id;
 
 public class ItemProvider extends ContentProvider{
 
@@ -19,6 +22,37 @@ public class ItemProvider extends ContentProvider{
     sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY,ItemContract.PATH_ITEMS + "/#", ITEM_ID);}
     private ItemDbHelper mDbHelper;
 
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match)
+        {case ITEMS:
+        return insert(uri,contentValues);
+        default:
+        throw new IllegalArgumentException("Insertion is not supported for " + uri); }}
+
+    private Uri insertItem(Uri uri,ContentValues values){
+    String name = values.getAsString(ItemContract.ItemEntry.COLUMN_ITEM_PRODUCT);
+    if (name==null){ throw new IllegalArgumentException("Item requires a name");}
+
+
+    String reference = values.getAsString(ItemContract.ItemEntry.COLUMN_ITEM_REFERENCE);
+    if (reference==null){ throw new IllegalArgumentException("Item requires a name");
+
+    String price = values.getAsString(ItemContract.ItemEntry.COLUMN_ITEM_PRICE);
+    if (price != null && price < 0) {
+    throw new IllegalArgumentException("Pet requires valid weight"); }
+
+    SQLiteDatabase database = mDbHelper.getWritableDatabase();
+    long id = database.insert(ItemContract.ItemEntry.TABLE_NAME, null, values);
+
+   if (id == -1) { Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null; }
+    getContext().getContentResolver().notifyChange(uri, null);
+    }
+        return ContentUris.withAppendedId(uri, id); }
+
+
 
     @Override
     public boolean onCreate() {
@@ -27,35 +61,27 @@ public class ItemProvider extends ContentProvider{
 
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,String sortOrder) {
     SQLiteDatabase database = mDbHelper.getReadableDatabase();
     Cursor cursor;
     int match = sUriMatcher.match(uri);
     switch (match) {
-        case ITEMS:
-            cursor=database.query(ItemContract.ItemEntry.TABLE_NAME,projection, selection, selectionArgs,
-            null, null, sortOrder);
-            break;
-        case ITEM_ID:
-        selection = ItemContract.ItemEntry._ID + "=?";
-        selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
-        cursor = database.query(ItemContract.ItemEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
-        break;
-        default:
-        throw new IllegalArgumentException("Cannot query unknown URI " + uri);}
-    return null;
-    }
+     case ITEMS:
+     cursor=database.query(ItemContract.ItemEntry.TABLE_NAME,projection, selection, selectionArgs,
+     null, null, sortOrder);
+     break;
 
-    @Override
-    public String getType(Uri uri) {
-        return null;
-    }
+    case ITEM_ID:
+    selection = ItemContract.ItemEntry._ID + "=?";
+    selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+    cursor = database.query(ItemContract.ItemEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+    break;
+    default: throw new IllegalArgumentException("Cannot query unknown URI " + uri);}
+    cursor.setNotificationUri(getContext().getContentResolver(), uri);
+    return cursor; }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
-    }
+
+
 
     @Override
     public int delete(Uri uri, String s, String[] strings) {
@@ -66,4 +92,14 @@ public class ItemProvider extends ContentProvider{
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
     }
-}
+
+    @Override
+    public String getType(Uri uri) {
+        final int match =sUriMatcher.match(uri);
+        switch (match){
+            case ITEMS:
+                return ItemContract.ItemEntry.CONTENT_LIST_TYPE;
+            case ITEM_ID:
+                return ItemContract.ItemEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);}}}
